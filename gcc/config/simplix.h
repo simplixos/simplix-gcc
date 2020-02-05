c18a2593214bec69f4c9ac8af55bb4ce4816f9ee
Aun-Ali Zaidi <admin@kodeit.net>
2020-02-05 16:05:18 -0600

    toolchain: Add initial Simplix support
    Adds the target i686-simplix and x86_64-simplix.
    
    Signed-off-by: Aun-Ali Zaidi <admin@kodeit.net>

---
+++ gcc/config/simplix.h
@@ -0,0 +1,30 @@
+/* Useful if you wish to make target-specific GCC changes. */
+#undef TARGET_SIMPLIX
+#define TARGET_SIMPLIX 1
+
+/* Default arguments */
+#undef LIB_SPEC
+#define LIB_SPEC "-lc" /* link against C standard library */
+
+/* Files that are linked before user code.
+   The %s tells GCC to look for these files in the library directory. */
+#undef STARTFILE_SPEC
+#define STARTFILE_SPEC "crt0.o%s crti.o%s crtbegin.o%s"
+
+/* Files that are linked after user code. */
+#undef ENDFILE_SPEC
+#define ENDFILE_SPEC "crtend.o%s crtn.o%s"
+
+/* Additional predefined macros. */
+#undef TARGET_OS_CPP_BUILTINS
+#define TARGET_OS_CPP_BUILTINS()			\
+	do {						\
+		builtin_define ("__simplix__");		\
+		builtin_define ("__SIMPLIX__");		\
+		builtin_define ("__simplix_os__");	\
+		builtin_define ("__SIMPLIX_OS__");	\
+		builtin_define ("__unix__");		\
+		builtin_assert ("system=simplix");	\
+		builtin_assert ("system=unix");		\
+		builtin_assert ("system=posix");	\
+	} while(0);
